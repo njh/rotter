34aba4b251aa7d31d979e44e9bb6735ba7ad7b6f
Nicholas J Humfrey <njh@aelius.com>
2010-10-28 20:24:31 +0100

    Added fix to autogen.sh, because git doesn't support empty directories.

--- autogen.sh
+++ autogen.sh
@@ -61,10 +61,14 @@ run_cmd() {
 }
 
 
+# Because git doesn't support empty directories
+if [ ! -d "$srcdir/build-scripts" ]; then
+	mkdir "$srcdir/build-scripts"
+fi 
+
 run_cmd aclocal
 run_cmd autoheader
 run_cmd automake --add-missing --copy
 run_cmd autoconf
-
 
 $srcdir/configure && echo
